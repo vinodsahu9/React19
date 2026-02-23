078f78d15494afa8b80a2d469763a386ba2d8376
Vinod Kumar Sahu <sahuvinod.mca2008@gmail.com>
2026-02-23 13:28:24 +0530

    Function and Component Diference

--- React19_Learning/src/App.jsx
+++ React19_Learning/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+// Component is a function that returns JSX (JavaScript XML)
+// which is a syntax extension for JavaScript
+// that looks similar to HTML.
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello, React 19! </h1>
     </>
-  )
+  );
 }
-
-export default App
+// Function starting with lowercase is not a component, it's just a regular function
+// and it can be used inside the component or outside the component, but it cannot be used as a component
+// because it does not return JSX and it does not follow the naming convention for components.
+// it return normal JavaScript value, in this case it returns the sum of two numbers.
+function sum(a, b) {
+  return a + b;
+}
+export default App;
